bef9758c89ae6ab9a8a1da788d60b015bb2654d5
sergio-teruel <sergio.teruel@tecnativa.com>
2025-09-03 09:00:16 +0200

    [FIX] stock_barcodes: Singleton error when user validate more than one picking

--- stock_barcodes/models/stock_picking.py
+++ stock_barcodes/models/stock_picking.py
@@ -64,9 +64,8 @@ class StockPicking(models.Model):
         if res is True and self.env.context.get("show_picking_type_action_list", False):
             res = self[:1].picking_type_id.get_action_picking_tree_ready()
 
-        if (
-            self.env.context.get("stock_barcodes_validate_picking", False)
-            and self.state == "done"
+        if self.env.context.get("stock_barcodes_validate_picking", False) and all(
+            p.state == "done" for p in self
         ):
             message_payload = {
                 "type": "actions_barcode",
