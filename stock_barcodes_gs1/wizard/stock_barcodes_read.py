a3342931a4c7b5aa0b47a170cf4c148582c31d67
Florian da Costa <florian.dacosta@akretion.com>
2025-09-08 22:39:13 +0200

    change 01 to 02

--- stock_barcodes_gs1/wizard/stock_barcodes_read.py
+++ stock_barcodes_gs1/wizard/stock_barcodes_read.py
@@ -123,7 +123,8 @@ class WizStockBarcodesRead(models.AbstractModel):
         return True
 
     def _hook_process_gs1_value(self, gs1_item):
-        """Hook to be extended by other modules"""
+        if gs1_item.get("ai") == "01":
+            gs1_item["ai"] = "02"
         return gs1_item["value"]
 
     @staticmethod
