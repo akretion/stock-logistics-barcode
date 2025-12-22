a45336b3a1775b83b068be81df46b10150e74dde
Florian da Costa <florian.dacosta@akretion.com>
2025-12-22 22:48:50 +0100

    [IMP] stock_barcodes: Avoid sending multiple scans at the same time

--- stock_barcodes/static/src/utils/barcode_handler_field.esm.js
+++ stock_barcodes/static/src/utils/barcode_handler_field.esm.js
@@ -9,9 +9,9 @@ patch(BarcodeHandlerField.prototype, {
     setup() {
         super.setup();
         const busService = useService("bus_service");
-        this.action = useService("action");
         this.orm = useService("orm");
         this.timer = null;
+        this.scanQueue = Promise.resolve();
 
         useEffect(() => {
             const handleBusNotification = (notif) => {
@@ -31,12 +31,24 @@ patch(BarcodeHandlerField.prototype, {
     async onBarcodeScanned(event) {
         // Super.onBarcodeScanned(...arguments);
         if (this.props.record.resModel.includes("wiz.stock.barcodes.read")) {
-            const result = await this.orm.call(
-                this.props.record.resModel,
-                "dummy_on_barcode_scanned",
-                [[this.props.record.resId], event.detail.barcode]
-            );
-            await this.props.record.load();
+            const barcode = event.detail.barcode;
+            const resModel = this.props.record.resModel;
+            const resId = this.props.record.resId;
+            this.scanQueue = this.scanQueue.then(async () => {
+                try {
+                    await this.orm.call(resModel, "dummy_on_barcode_scanned", [
+                        [resId],
+                        barcode,
+                    ]);
+                    await this.props.record.load();
+                } catch (error) {
+                    console.error("Erreur durant le traitement du scan:", error);
+                    await this.props.record.load();
+                }
+            });
+            await this.scanQueue;
+        } else {
+            super.onBarcodeScanned(...arguments);
             // Option if we want to delay the web_read so the guy can scan, scan, scan and screen only refresh when he does not scan for some time.
             //            try {
             //                const result = await this.orm.call("wiz.stock.barcodes.read.picking", "dummy_on_barcode_scanned", [[this.props.record.resId], event.detail.barcode])
